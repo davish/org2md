992e71ec0416f822f9e4ec97a67780ede3b362ba
Davis Haupt <davis.haupt@gmail.com>
2023-12-26 19:44:01 -0500

    add another test

--- test/parser.ml
+++ test/parser.ml
@@ -24,3 +24,15 @@ let%expect_test "" =
         (Plain_text a) (Plain_text world))))) |}];
   ()
 ;;
+
+let%expect_test "" = 
+  let output = Parser.parse "this is a *test of org-mode* syntax" in
+  print_s [%sexp (output : (Ast.t, string) Result.t)];
+  [%expect {|
+    (Ok
+     (Element
+      (Paragraph
+       ((Plain_text this) (Plain_text is) (Plain_text a)
+        (Markup Bold "test of org-mode") (Plain_text syntax))))) |}];
+  ();
+;;
